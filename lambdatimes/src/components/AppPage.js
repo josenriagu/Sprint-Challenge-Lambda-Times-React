008ab591fc8a8fd4d270e3965c4ed33f9dcb960e
Josemaria <josemarianriagu25@gmail.com>
2019-06-07 17:06:06 +0800

    Refactor Content component to show only on login

---
+++ lambdatimes/src/components/AppPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AppPageDiv } from '../AppStyles';
+import Content from './Content/Content';
+
+const AppPage = () => {
+    return (
+        <AppPageDiv>
+            <Content />
+        </AppPageDiv>
+    );
+}
+
+export default AppPage;
