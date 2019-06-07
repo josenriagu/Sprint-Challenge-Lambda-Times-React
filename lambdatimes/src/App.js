40f32eb94d1b5bddc9da642195284d74c4462c59
Josemaria <josemarianriagu25@gmail.com>
2019-06-07 17:04:18 +0800

    Refactor to class component Use styled-components Define state and method for login

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import withAuthenticate from './components/authentication/withAuthenticate';
+import { AppDiv } from './AppStyles';
+import AppPage from './components/AppPage';
+import Login from './components/Login/Login';
 import TopBar from './components/TopBar';
 import Header from './components/Header';
-import Content from './components/Content/Content';
 
-const App = () => {
-  return (
-    <div className="App">
-      <TopBar />
-      <Header />
-      <Content />
-    </div>
-  );
+const ComponentFromWithAuthenticate = withAuthenticate(AppPage)(Login);
+
+export default class App extends Component {
+  state = { show: false }
+  
+  toggleShow = () => {
+    this.setState({show: true})
+  }
+  render() {
+    return (
+      <AppDiv>
+        <TopBar toggleShow={this.toggleShow}/>
+        <Header />
+        <ComponentFromWithAuthenticate show={this.state.show}/>
+      </AppDiv>
+    );
+  }
 }
-
-export default App;
