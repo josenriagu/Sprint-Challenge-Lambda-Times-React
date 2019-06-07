9797c85eea57ad16688f0eff0a944ae7d67d515a
Josemaria <josemarianriagu25@gmail.com>
2019-06-07 15:53:35 +0800

    Refactor to use styled-components

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { HeaderDiv, DateSpan, HeaderH1, TimeSpan } from './Styles/HeaderStyles';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
 
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
+    <HeaderDiv>
+      <DateSpan>MARCH 32, 2018</DateSpan>
+      <HeaderH1>Lambda Times</HeaderH1>
+      <TimeSpan className="temp">98°</TimeSpan>
+    </HeaderDiv>
   )
 }
 
