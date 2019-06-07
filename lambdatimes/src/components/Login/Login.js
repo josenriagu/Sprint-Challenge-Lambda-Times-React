e7121875e12ee9406b1bd90a7140d9c3eb196d80
Josemaria <josemarianriagu25@gmail.com>
2019-06-07 16:59:58 +0800

    [Stretch] Create Login component Define modal behaviour based on passed in state prop [Not using react-strap]

---
+++ lambdatimes/src/components/Login/Login.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: ""
+        }
+    }
+    changeHandler = event => {
+        this.setState({ ...this.state, username: event.target.value })
+    }
+    login = () => {
+        localStorage.setItem("lambda", JSON.stringify(this.state.username));
+        window.location.reload();
+    }
+    render() {
+        const loginStyle = this.props.show ? "modal display-block" : "modal display-none";
+        const loginPrompt = this.props.show ? "display-none" : "display-block";
+        // initial definition
+        // if (this.props.show === false) {
+        //     return (
+        //         <div>
+        //             <h2>Protected Content!!!</h2>
+        //             <h3>Please log in to continue</h3>
+        //         </div>
+        //     )
+        // }
+        return (
+            <div>
+                {/* revised definition */}
+                <div className={loginPrompt}>
+                    <h2>Protected Content!!!</h2>
+                    <h3>Please log in to continue</h3>
+                </div>
+                <div className={loginStyle}>
+                    <form
+                        className="form-login"
+                        onSubmit={this.login}>
+                        <div className="img-container">
+                            <img src="/assets/lambda.png" />
+                        </div>
+                        <div className="field">
+                            <input
+                                type="text"
+                                onChange={(event) => this.changeHandler(event)}
+                                placeholder="Enter your username"
+                                required
+                            />
+                        </div>
+                        <div className="field">
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                required
+                            />
+                        </div>
+                        {
+                            (this.state.username.length !== 0)
+                                ?
+                                <button type="submit">Log In</button>
+                                :
+                                <div></div>
+                        }
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
